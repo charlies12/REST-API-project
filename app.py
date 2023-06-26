d0b6fac07657f4059152202e274c488f098f4f78
Charlie Sanchez <charlie@Charlies-MBP-2.attlocal.net>
2023-06-25 21:46:19 -0700

    Add model changes and migrations

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 import secrets
 
+import psycopg2
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 
 from db import db
 from blocklist import BLOCKLIST
@@ -18,6 +20,7 @@ from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
